75013698d5cc159c13f81ffa86921de5c2c9cda9
Jacob Kring <jacob.kring.71@gmail.com>
2021-08-16 13:04:32 -0400

    remove genuine header check

--- elasticsearch.go
+++ elasticsearch.go
@@ -251,9 +251,11 @@ func (c *Client) doProductCheck(f func() error) error {
 // genuineCheckHeader validates the presence of the X-Elastic-Product header
 //
 func genuineCheckHeader(header http.Header) error {
-	if header.Get("X-Elastic-Product") != "Elasticsearch" {
-		return errors.New(unknownProduct)
-	}
+	/*
+		if header.Get("X-Elastic-Product") != "Elasticsearch" {
+			return errors.New(unknownProduct)
+		}
+	 */
 	return nil
 }
 
